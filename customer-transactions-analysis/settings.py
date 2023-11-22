87a8d814258cd6c8c3e27f8ee0a5e21c0af0e68e
esfandiaryfard <anahita.esfandiaryfard@gmail.com>
2023-11-22 01:20:29 +0100

    :bug: Fix bug in project setting

--- customer-transactions-analysis/settings.py
+++ customer-transactions-analysis/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoProject.urls'
+ROOT_URLCONF = 'customer-transactions-analysis.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djangoProject.wsgi.application'
+WSGI_APPLICATION = 'customer-transactions-analysis.wsgi.application'
 
 
 # Database
